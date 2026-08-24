d9b2b7291e9d6d598c833acb955100ff4aa7a728
Guren <71011437+Gen67@users.noreply.github.com>
2026-08-24 15:01:01 +0800

    Time: 4 ms (73.68%) | Memory: 61 MB (58.20%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.js
+++ 13-roman-to-integer/roman-to-integer.js
@@ -3,19 +3,30 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    const value = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M:1000 }; 
-    let res = 0;
-    for(let i = 0; i < s.length; i++ ){ 
-        const current = value[s [i]];
-        const next = value[s [i + 1 ]];
-         if(current < next){ 
-            res -= current
-        } else { 
-            res += current; 
+    
+    const value = 
+    {
+    I:1,
+    V:5,
+    X:10,
+    L:50,
+    C:100,
+    D:500,
+    M:1000};
+
+ let res = 0; 
+    for(let i = 0; i < s.length; i++){ 
+        const current = value[s[i]];
+        const next = value[s[i + 1]]; 
+       
+        if(current < next){ 
+            res -= current;
+        } else{ 
+            res += current;
         }
+        
     }
 
-   return res; 
-   
-   console.log(romanToInt(s));
+    return res;
+  
 };
